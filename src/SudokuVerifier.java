8e76d2784a9631dd592c5af71bc2d3d87af651cf
somename <someemail>
2016-09-15 15:36:49 +0300

    besouro automatic message

--- src/SudokuVerifier.java
+++ src/SudokuVerifier.java
@@ -4,37 +4,21 @@ public class SudokuVerifier {
 	public int verify(String candidateSolution) {
 		// returns 0 if the candidate solution is correct
 
-		int temp;
-		
-		if(candidateSolution.length()!=81){
-			return -5;
+		//Que mida 81 y este entre 1 y 9
 
-		}else{
-
-			for (int i = 0; i < 9; i++) {
-				
-				
-				temp = Integer.parseInt(candidateSolution); 
-				
-				 if(candidateSolution.charAt(i) > 0){
-					
-					
-					for (int j = 0; j < 9; j++) {
-						if(candidateSolution.charAt(j) > 0){
-						temp = Integer.parseInt(candidateSolution); 
-						}else{
-							return -1;
-						}
-					}
-				}else{
-					return -1;
-				}
-			}
+		int i = 0;
+		while ((candidateSolution.charAt(i)>=1 && candidateSolution.charAt(i)<=9)&& i!=81) {	
+			i++;
 		}
-
-
-		return 0;
-
-		//check
+		if(i == 81){
+			return 1;
+		}else{
+			return -1;
+		}
 	}
+
+
+	//check
 }
+
+
